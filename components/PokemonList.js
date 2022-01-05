e4ca107540e0adccdfd295212ba56a40a28fcbd6
Yann <yann.pomie@gmail.com>
2022-01-05 13:57:29 +0100

    added aditionnal fecthing on scroll

--- components/PokemonList.js
+++ components/PokemonList.js
@@ -4,19 +4,18 @@ app.component('pokemon-list',
     /*html*/
     `
     <ul class="pokelist">
-        <li v-for="p in pokemonArray" :key="p.id"><pokemon-card :pokemon="p"></pokemon-card></li>-->
+        <li v-for="p in pokemonArray" :key="p.id">
+            <pokemon-card :pokemon="p"></pokemon-card> 
+        </li>
     </ul>
     `,
     data() {
         return {
             pokemonArray: [],
-            offset : -20 // so we can start at 20 on the frst iteration
+            offset : -20 // so we can start at 20 on the first iteration
         }
     },
     methods : {
-        echo() {
-            console.log("echo")
-        },
         fetchList() {
             this.offset += 20
             return fetch(`https://pokeapi.co/api/v2/pokemon?limit=20&offset=${this.offset}`).then(function(data){
@@ -29,6 +28,15 @@ app.component('pokemon-list',
         this.fetchList().then(function(data) {
             that.pokemonArray = data.results
         })
-        this.echo()
+        window.onscroll = function(e) {
+            if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+                that.fetchList().then(function(data) {
+                    for (let i= 0; i < data.results.length; ++i){
+                        that.pokemonArray.push(data.results[i])
+                        //TODO (Optionnal) Consider checking if piture is available because some pokemons return a 404
+                    }
+                })
+            }
+        };
     }
 })
