79a657b37a64a0a3721436ada8c3c3500259dd94
Yann <yann.pomie@gmail.com>
2022-01-05 16:41:01 +0100

    linked pokemon-list to pokemon-popup

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 const app = Vue.createApp({
     data() {
         return {
+            askedId : 0,
+            showPopup : false
         }
     },
     methods: {
+        show(payload) {
+            //console.log("passage par la racine")
+            this.askedId = Number(payload.id)
+            this.showPopup = true
+        },
+        closePopup() {
+            this.showPopup = false
+        }
     }
 })
